c6d2e447f4d29bc111f068f6ceb44ad65e76746d
adhpawal <adhpawal@gmail.com>
2022-05-05 17:53:02 +0545

    Adds System.out.println rule

--- src/main/java/checks/PersonalDataRule.java
+++ src/main/java/checks/PersonalDataRule.java
@@ -12,7 +12,29 @@ import java.util.List;
 public class PersonalDataRule extends IssuableSubscriptionVisitor {
 
     @Override
-    public List<Kind> nodesToVisit() {
-        return Collections.emptyList();
+    public List<Tree.Kind> nodesToVisit() {
+        return Collections.singletonList(Tree.Kind.MEMBER_SELECT);
+    }
+
+    @Override
+    public void visitNode(Tree tree) {
+        MemberSelectExpressionTree mset = (MemberSelectExpressionTree) tree;
+        if (isOutOrErr(mset) && isSystem(mset.expression())) {
+            reportIssue(tree, "Replace this use of System.out or System.err by a logger.");
+        }
+    }
+
+    private static boolean isSystem(ExpressionTree expression) {
+        IdentifierTree identifierTree = null;
+        if (expression.is(Tree.Kind.IDENTIFIER)) {
+            identifierTree = (IdentifierTree) expression;
+        } else if (expression.is(Tree.Kind.MEMBER_SELECT)) {
+            identifierTree = ((MemberSelectExpressionTree) expression).identifier();
+        }
+        return identifierTree != null && "System".equals(identifierTree.name());
+    }
+
+    private static boolean isOutOrErr(MemberSelectExpressionTree mset) {
+        return "out".equals(mset.identifier().name()) || "err".equals(mset.identifier().name());
     }
 }
